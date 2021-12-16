76696bbeadb6a72a5a5a436814d218f529d653b6
Alex Burner <aburner@apple.com>
2021-12-15 18:15:32 -0800

    Update ButtonAnchor to use URL #hash

--- components/blocks/ButtonAnchor.tsx
+++ components/blocks/ButtonAnchor.tsx
@@ -3,15 +3,22 @@ import { BlogPostBySlug_blogPost_blocks_ButtonAnchorRecord } from '../../gql/typ
 
 export const ButtonAnchor: FC<{
   block: BlogPostBySlug_blogPost_blocks_ButtonAnchorRecord
-}> = ({ block }) => (
-  <button
-    className="button is-primary is-medium"
-    onClick={() =>
-      document
-        .getElementById(block.anchorId ?? '')
-        ?.scrollIntoView({ behavior: 'smooth' })
-    }
-  >
-    {block.label}
-  </button>
-)
+}> = ({ block }) => {
+  const anchorId = block.anchorId ?? ''
+  const anchorHash = `#${anchorId}`
+  return (
+    <a
+      className="button is-primary is-medium"
+      href={anchorHash}
+      onClick={(e) => {
+        e.preventDefault()
+        window.history.replaceState(null, '', anchorHash)
+        document
+          .getElementById(anchorId)
+          ?.scrollIntoView({ behavior: 'smooth' })
+      }}
+    >
+      {block.label}
+    </a>
+  )
+}
